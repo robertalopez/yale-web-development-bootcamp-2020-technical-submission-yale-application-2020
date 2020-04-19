33b7da21c6d143d45ffc11d643453acf59c5a33e
Robert Lopez <robert.lopez@yale.edu>
2020-04-19 05:02:35 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,6 +2,13 @@ require_relative './test'
 AVERAGE_HEIGHT_IN_INCHES = 67
 
 # Your Code Here!
+name = "robert"
+age = 20 
+favorite_food = "enchilada"
+height_in_inches = 72
+height_message = height_in_inches < AVERAGE_HEIGHT_IN_INCHES ? "You are not taller than average" : "You are taller than average"
+
+
 
 ## Do not modify below this line!
 
